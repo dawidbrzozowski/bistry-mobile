51b77cf4a2bc4d7a954ab965cfbb6fbf218e935f
Dawid Brzozowski <dawidbrzozowski1998@gmail.com>
2022-05-06 19:41:08 +0200

    navigation, but qrcode does not work

--- bistry-app/App.js
+++ bistry-app/App.js
@@ -1,38 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
+import {StatusBar} from "expo-status-bar";
+import OutlinedButton from "./components/UI/OutlinedButton";
+import BarScanner from "./components/qrcode/BarScanner";
+import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+function HomeScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Bistry | Witaj w restauracji</Text>
+            <Button
+                title="Zeskanuj kod QR!"
+                onPress={() => navigation.navigate('QRCodeScanner')}
+            />
+        </View>
+    );
+}
+
 
 export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
-
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-  };
-
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
+const Stack = createNativeStackNavigator();
 
   return (
-      <View style={styles.container}>
-        <BarCodeScanner
-            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-            style={StyleSheet.absoluteFillObject}
-        />
-        {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
-      </View>
+      <>
+        <StatusBar style="dark"/>
+          <NavigationContainer>
+              <Stack.Navigator initialRouteName="Home">
+                  <Stack.Screen name="Home" component={HomeScreen} />
+                  <Stack.Screen name="QRCodeScanner" component={BarScanner}/>
+              </Stack.Navigator>
+          </NavigationContainer>
+      </>
+
   );
 }
 
---
+++ bistry-app/components/qrcode/BarScanner.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import {StatusBar} from "expo-status-bar";
+
+function BarScanner({ navigation }) {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
+
+    const handleBarCodeScanned = ({ type, data }) => {
+        setScanned(true);
+        alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    };
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
+
+
+    return (
+        <>
+            <StatusBar style="dark"/>
+            <View style={styles.container}>
+                <BarCodeScanner
+                    onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                    style={StyleSheet.absoluteFillObject}
+                />
+                {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+            </View>
+        </>
+
+    );
+}
+
+export default BarScanner;
+
+const styles = StyleSheet.create({
+    imagePreview: {
+        width: '100%',
+        height: 200,
+        marginVertical: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ffffff',
+        borderRadius: 4,
+        overflow: 'hidden',
+    },
+});
